9ad568e512ed01c93ec895d15173858652367565
euhelolo <129217350+euhelolo@users.noreply.github.com>
2023-09-12 10:31:00 -0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 function TocaSomToim() {
     document.querySelector('#som_tecla_toim').play();
 }
+
+const ListaDeSons = document.querySelectorAll('.tecla');
+
 document.querySelector('.tecla_toim').onclick = TocaSomToim; 
