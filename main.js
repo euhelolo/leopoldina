cfd572d9f647229c56e8bc307bb4b960b881003b
euhelolo <129217350+euhelolo@users.noreply.github.com>
2023-09-19 10:13:49 -0300

    b

--- main.js
+++ main.js
@@ -9,9 +9,10 @@ let contador = 0;
 
 while (contador < 9) {
 ListaDeSons[5].onclick = TocaSomToim;
-
+lista[contaor].onclick = tocar;
 const classe= lista [1].classList[1];
-console.log(classe);contador = contador + 1;
+console.log(classe);
+contador = contador + 1;
 
 console.log(contador);
 
