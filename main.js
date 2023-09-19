51ff8bfeb4653e64c53cf05691626e98fa9e993a
euhelolo <129217350+euhelolo@users.noreply.github.com>
2023-09-19 10:18:18 -0300

    a

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const lista = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (contador < 9) 
-lista[contador].onclick = tocar;
+lista[contador].onclick = tocar('#som_tecla_toim');
 const classe= lista [1].classList[1];
 console.log(classe);
 contador = contador + 1;
