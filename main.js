96b46dbb6d0e99bf73b87685313b0e85e87bc08e
euhelolo <129217350+euhelolo@users.noreply.github.com>
2023-11-10 10:08:53 -0300

    W

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const lista = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
-for(contador < lista.length) {
+for(let contador = 0;contador < lista.length;contador = contador + 1;) {
     const tecla = lista[contador];
     const classe = tecla.classList[1];
     console.log(classe);
